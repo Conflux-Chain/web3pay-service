99d0841e0fb368259b5dfcf0df82b766f20c7b02
wanliqun <wanliqunn@163.com>
2022-07-21 17:07:26 +0800

    Load .env file from current dir to override viper settings (#8)

--- config/config.go
+++ config/config.go
@@ -2,6 +2,7 @@ package config
 
 import (
 	utilcfg "github.com/Conflux-Chain/go-conflux-util/config"
+	"github.com/subosito/gotenv"
 )
 
 // this can be overwriten when go build
@@ -9,4 +10,7 @@ var EnvPrefix string = "web3pay"
 
 func init() {
 	utilcfg.MustInit(EnvPrefix)
+
+	// load environment variables from .env file from current directory
+	gotenv.Load()
 }
