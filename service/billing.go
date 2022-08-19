5959c524d7949abb0fa933b26fc30f05404afff9
wanliqun <wanliqunn@163.com>
2022-08-19 11:07:27 +0800

    Add batch charge JSON-RPC method (#29)

--- service/billing.go
+++ service/billing.go
@@ -1,13 +1,11 @@
 package service
 
 import (
-	"fmt"
-	"math/big"
-
 	"github.com/Conflux-Chain/web3pay-service/model"
 	"github.com/Conflux-Chain/web3pay-service/store/sqlite"
 	"github.com/Conflux-Chain/web3pay-service/util"
 	"github.com/ethereum/go-ethereum/common"
+	"github.com/shopspring/decimal"
 	"github.com/sirupsen/logrus"
 	"golang.org/x/net/context"
 	"gorm.io/gorm"
@@ -29,10 +27,21 @@ type BillingChargeReceipt struct {
 	Fee        string `json:"fee"`
 }
 
+type BillingChargeBatchRequest struct {
+	ResourceUses map[string]int64 `json:"resourceUses"`
+	DryRun       bool             `json:"dryRun"`
+	AppCoin      common.Address   `json:"-"`
+	Customer     common.Address   `json:"-"`
+}
+
+type BillingChargeBatchReceipt struct {
+	ResourceFees map[string]decimal.Decimal `json:"resourceFees"`
+}
+
 type BillingService struct {
 	chainSvc    *BlockchainService
 	sqliteStore *sqlite.SqliteStore
-	// map{ bill ID => map{ resource index = > API call times } }
+	// map{ bill ID => map{ resource index = > use count } }
 	billingStatements map[uint64]map[uint32]int64
 }
 
@@ -44,25 +53,38 @@ func NewBillingService(sqliteStore *sqlite.SqliteStore, chainSvc *BlockchainServ
 	}
 }
 
-func (bs *BillingService) Charge(ctx context.Context, req *BillingChargeRequest) (*BillingChargeReceipt, error) {
-	logger := logrus.WithField("request", req)
+func (bs *BillingService) ChargeBatch(ctx context.Context, req *BillingChargeBatchRequest) (*BillingChargeBatchReceipt, error) {
+	var totalCost decimal.Decimal
+	resourceCosts := make(map[string]decimal.Decimal)
+	statements := make(map[uint32]int64)
 
-	resource, err := bs.chainSvc.GetAppCoinResourceWithId(req.AppCoin, req.ResourceId)
-	if err != nil {
-		logger.WithError(err).Debug("Billing charge failed to retrieve APP coin resource")
-		return nil, err
+	for resourceId, cnt := range req.ResourceUses {
+		resource, err := bs.chainSvc.GetAppCoinResourceWithId(req.AppCoin, resourceId)
+		if err != nil {
+			logrus.WithFields(logrus.Fields{
+				"resourceId": resourceId,
+				"appCoin":    req.AppCoin,
+			}).WithError(err).Debug("Billing charge failed to retrieve APP coin resource")
+			return nil, err
+		}
+
+		delta := decimal.NewFromBigInt(resource.Weight, 0).Mul(decimal.NewFromInt(cnt))
+		cost := resourceCosts[resource.ResourceId].Add(delta)
+		resourceCosts[resource.ResourceId] = cost
+
+		statements[resource.Index] += cnt
+		totalCost = totalCost.Add(delta)
 	}
 
-	fee := resource.Weight
-	logger = logger.WithField("fee", fee.String())
+	logger := logrus.WithFields(logrus.Fields{
+		"request":       req,
+		"totalCost":     totalCost,
+		"resourceCosts": resourceCosts,
+	})
 
-	if fee.Cmp(big.NewInt(0)) <= 0 { // no fee charged
-		logger.WithError(err).Debug("Billing charge skipped with no fee to be charged")
-
-		return &BillingChargeReceipt{
-			ResourceId: resource.ResourceId,
-			Fee:        fmt.Sprintf("%v", fee.String()),
-		}, nil
+	if totalCost.IsZero() { // no fee charged
+		logger.Debug("Billing charge skipped with no fee to be charged")
+		return &BillingChargeBatchReceipt{ResourceFees: resourceCosts}, nil
 	}
 
 	// get account status
@@ -81,17 +103,14 @@ func (bs *BillingService) Charge(ctx context.Context, req *BillingChargeRequest)
 	}
 
 	// insufficient balance?
-	if appCoinAccount.TotalBalance().Cmp(fee) < 0 {
+	if appCoinAccount.TotalBalance().Cmp(totalCost.BigInt()) < 0 {
 		logger.Debug("Billing charge skipped due to insufficient balance")
 		return nil, model.ErrInsufficentBalance
 	}
 
 	if req.DryRun { // for simulation only?
 		logger.Debug("Billing charge skipped for dry run")
-		return &BillingChargeReceipt{
-			ResourceId: resource.ResourceId,
-			Fee:        fmt.Sprintf("%v", fee.String()),
-		}, nil
+		return &BillingChargeBatchReceipt{ResourceFees: resourceCosts}, nil
 	}
 
 	util.KLock(KeyBillingChargeLocker)
@@ -99,6 +118,8 @@ func (bs *BillingService) Charge(ctx context.Context, req *BillingChargeRequest)
 
 	coin, addr := appCoinAccount.Coin, appCoinAccount.Address
 	if err = bs.sqliteStore.Transaction(func(tx *gorm.DB) error {
+		fee := totalCost.BigInt()
+
 		bill, err := bs.sqliteStore.UpsertBill(tx, coin, addr, fee)
 		if err != nil {
 			logger.WithError(err).Error("Billing charge failed to upsert bill")
@@ -112,7 +133,7 @@ func (bs *BillingService) Charge(ctx context.Context, req *BillingChargeRequest)
 		}
 
 		if deducted {
-			bs.recordApiCallOnce(bill.ID, resource.Index)
+			bs.collectBillStatements(bill.ID, statements)
 		}
 
 		return nil
@@ -120,10 +141,28 @@ func (bs *BillingService) Charge(ctx context.Context, req *BillingChargeRequest)
 		return nil, err
 	}
 
-	return &BillingChargeReceipt{
-		ResourceId: resource.ResourceId,
-		Fee:        fee.String(),
-	}, nil
+	return &BillingChargeBatchReceipt{ResourceFees: resourceCosts}, nil
+}
+
+func (bs *BillingService) Charge(ctx context.Context, req *BillingChargeRequest) (*BillingChargeReceipt, error) {
+	batchReceipt, err := bs.ChargeBatch(ctx, &BillingChargeBatchRequest{
+		ResourceUses: map[string]int64{req.ResourceId: 1},
+		DryRun:       req.DryRun,
+		AppCoin:      req.AppCoin,
+		Customer:     req.Customer,
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	for resourceId, fee := range batchReceipt.ResourceFees {
+		return &BillingChargeReceipt{
+			ResourceId: resourceId,
+			Fee:        fee.String(),
+		}, nil
+	}
+
+	return nil, nil
 }
 
 func (bs *BillingService) GetAndDelStatements(billID uint64) map[uint32]int64 {
@@ -133,10 +172,13 @@ func (bs *BillingService) GetAndDelStatements(billID uint64) map[uint32]int64 {
 	return statements
 }
 
-func (bs *BillingService) recordApiCallOnce(billID uint64, resourceIndex uint32) {
+func (bs *BillingService) collectBillStatements(billID uint64, resourceStatements map[uint32]int64) {
 	if _, ok := bs.billingStatements[billID]; !ok {
-		bs.billingStatements[billID] = make(map[uint32]int64)
+		bs.billingStatements[billID] = resourceStatements
+		return
 	}
 
-	bs.billingStatements[billID][resourceIndex]++
+	for resourceIndex, useCnt := range resourceStatements {
+		bs.billingStatements[billID][resourceIndex] += useCnt
+	}
 }
