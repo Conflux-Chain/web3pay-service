e18da515880a6ba4a354787b391a33789681b431
wanliqun <wanliqunn@163.com>
2022-08-25 10:50:49 +0800

    Add JSON-RPC billed service demo (#33)

---
+++ cmd/demo.go
@@ -0,0 +1,133 @@
+package cmd
+
+import (
+	"strings"
+
+	viperutil "github.com/Conflux-Chain/go-conflux-util/viper"
+	"github.com/Conflux-Chain/web3pay-service/client"
+	"github.com/Conflux-Chain/web3pay-service/demo"
+	"github.com/sirupsen/logrus"
+	"github.com/spf13/cobra"
+	"github.com/spf13/viper"
+)
+
+type demoConfig struct {
+	client.ClientConfig `mapstructure:",squash"`
+	CustomerKey         string
+	ServerPort          int
+	RpcStyle            string
+}
+
+var (
+	demoConf = demoConfig{}
+
+	demoCmd = &cobra.Command{
+		Use:   "demo",
+		Short: "Run billed service provider and consumer demo",
+		Run: func(cmd *cobra.Command, args []string) {
+			cmd.Help()
+		},
+	}
+
+	demoProviderCmd = &cobra.Command{
+		Use:   "provider",
+		Short: "Run demo billed service provider",
+		Run:   startProvider,
+	}
+
+	demoConsumerCmd = &cobra.Command{
+		Use:   "consumer",
+		Short: "Run demo billed service consumer",
+		Run:   startConsumer,
+	}
+)
+
+func init() {
+	demoCmd.AddCommand(demoProviderCmd)
+	demoCmd.AddCommand(demoConsumerCmd)
+	rootCmd.AddCommand(demoCmd)
+
+	flags := demoCmd.PersistentFlags()
+
+	// billing gateway URL
+	flags.StringVarP(
+		&demoConf.Gateway, "gateway", "g", "", "billing gateway URL",
+	)
+	viper.BindPFlag("demo.gateway", flags.Lookup("gateway"))
+
+	// billing key
+	flags.StringVarP(
+		&demoConf.BillingKey, "billing-key", "b", "",
+		"billing key (use 'ecrecover' command to generate one)",
+	)
+	viper.BindPFlag("demo.billingKey", flags.Lookup("billing-key"))
+
+	// customer key
+	flags.StringVarP(
+		&demoConf.CustomerKey, "customer-key", "c", "",
+		"customer key (use 'ecrecover' command to generate one)",
+	)
+	viper.BindPFlag("demo.customerKey", flags.Lookup("customer-key"))
+
+	// demo provider server port
+	flags.IntVarP(
+		&demoConf.ServerPort, "port", "p", 38258,
+		"listening port for the demo billed service provider",
+	)
+	viper.BindPFlag("demo.serverPort", flags.Lookup("port"))
+
+	// RPC style
+	flags.StringVarP(
+		&demoConf.RpcStyle, "style", "s", "jsonrpc",
+		"RPC style for the billed service demo, available options are 'jsonrpc' and 'restful'",
+	)
+	viper.BindPFlag("demo.rpcStyle", flags.Lookup("style"))
+}
+
+func startProvider(cmd *cobra.Command, args []string) {
+	// load config from env vars or flags
+	viperutil.MustUnmarshalKey("demo", &demoConf)
+
+	if len(demoConf.Gateway) == 0 {
+		logrus.Info("Billing gateway must be provided")
+		return
+	}
+
+	if len(demoConf.BillingKey) == 0 {
+		logrus.Info("Billing key must be provided")
+		return
+	}
+
+	if strings.EqualFold(demoConf.RpcStyle, "jsonrpc") {
+		logrus.WithField("listenPort", demoConf.ServerPort).
+			Info("Starting demo JSON-RPC billed service provider...")
+
+		err := demo.RunJsonRpcServiceProvider(demoConf.ClientConfig, demoConf.ServerPort)
+		if err != nil {
+			logrus.WithField("demoConfig", demoConf).
+				Info("Failed to serve JSON-RPC billed service provider")
+		}
+	}
+}
+
+func startConsumer(cmd *cobra.Command, args []string) {
+	// load config from env vars or flags
+	viperutil.MustUnmarshalKey("demo", &demoConf)
+
+	if len(demoConf.CustomerKey) == 0 {
+		logrus.Info("Customer key must be provided")
+		return
+	}
+
+	if strings.EqualFold(demoConf.RpcStyle, "jsonrpc") {
+		resp, err := demo.RunJsonRpcServiceConsumer(demoConf.CustomerKey, demoConf.ServerPort)
+		if err != nil {
+			logrus.WithField("demoConfig", demoConf).
+				WithError(err).
+				Info("Failed to run demo billed JSON-RPC service consumer")
+			return
+		}
+
+		logrus.WithField("response", resp).Info("Run demo billed JSON-RPC service consumer")
+	}
+}
--- go.mod
+++ go.mod
@@ -20,6 +20,7 @@ require (
 	github.com/shopspring/decimal v1.3.1
 	github.com/sirupsen/logrus v1.8.1
 	github.com/spf13/cobra v1.5.0
+	github.com/spf13/viper v1.10.0
 	github.com/stretchr/testify v1.7.0
 	github.com/subosito/gotenv v1.2.0
 	go.uber.org/multierr v1.1.0
@@ -40,6 +41,7 @@ require (
 	github.com/deepmap/oapi-codegen v1.8.2 // indirect
 	github.com/fatih/color v1.13.0 // indirect
 	github.com/felixge/httpsnoop v1.0.1 // indirect
+	github.com/fjl/memsize v0.0.0-20190710130421-bcb5799ab5e5 // indirect
 	github.com/fsnotify/fsnotify v1.5.1 // indirect
 	github.com/gammazero/deque v0.1.0 // indirect
 	github.com/go-ole/go-ole v1.2.1 // indirect
@@ -48,14 +50,17 @@ require (
 	github.com/google/go-cmp v0.5.7 // indirect
 	github.com/google/uuid v1.3.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
+	github.com/hashicorp/go-bexpr v0.1.10 // indirect
 	github.com/hashicorp/go-immutable-radix v1.3.1 // indirect
 	github.com/hashicorp/hcl v1.0.0 // indirect
 	github.com/holiman/bloomfilter/v2 v2.0.3 // indirect
 	github.com/holiman/uint256 v1.2.0 // indirect
+	github.com/huin/goupnp v1.0.2 // indirect
 	github.com/inconshreveable/mousetrap v1.0.0 // indirect
 	github.com/influxdata/influxdb v1.8.3 // indirect
 	github.com/influxdata/influxdb-client-go/v2 v2.4.0 // indirect
 	github.com/influxdata/line-protocol v0.0.0-20210311194329-9aa0e372d097 // indirect
+	github.com/jackpal/go-nat-pmp v1.0.2 // indirect
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/klauspost/compress v1.14.1 // indirect
@@ -66,6 +71,7 @@ require (
 	github.com/mattn/go-sqlite3 v1.14.12 // indirect
 	github.com/miguelmota/go-ethereum-hdwallet v0.1.1 // indirect
 	github.com/mitchellh/mapstructure v1.4.3 // indirect
+	github.com/mitchellh/pointerstructure v1.2.0 // indirect
 	github.com/olekukonko/tablewriter v0.0.5 // indirect
 	github.com/openweb3/go-sdk-common v0.0.0-20220524083215-d22d44765e44 // indirect
 	github.com/pelletier/go-toml v1.9.4 // indirect
@@ -73,12 +79,12 @@ require (
 	github.com/prometheus/tsdb v0.7.1 // indirect
 	github.com/rjeczalik/notify v0.9.1 // indirect
 	github.com/royeo/dingrobot v1.0.1-0.20191230075228-c90a788ca8fd // indirect
+	github.com/rs/cors v1.7.0 // indirect
 	github.com/shirou/gopsutil v3.21.4-0.20210419000835-c7a38de76ee5+incompatible // indirect
 	github.com/spf13/afero v1.6.0 // indirect
 	github.com/spf13/cast v1.4.1 // indirect
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
-	github.com/spf13/viper v1.10.0 // indirect
 	github.com/syndtr/goleveldb v1.0.1-0.20210819022825-2ae1ddf74ef7 // indirect
 	github.com/tklauser/go-sysconf v0.3.5 // indirect
 	github.com/tklauser/numcpus v0.2.2 // indirect
@@ -87,11 +93,13 @@ require (
 	github.com/valyala/fasthttp v1.33.0 // indirect
 	go.uber.org/atomic v1.3.2 // indirect
 	golang.org/x/crypto v0.0.0-20220112180741-5e0467b6c7ce // indirect
+	golang.org/x/sync v0.0.0-20210220032951-036812b2e83c // indirect
 	golang.org/x/sys v0.0.0-20220405052023-b1e9470b6e64 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	gopkg.in/ini.v1 v1.66.2 // indirect
 	gopkg.in/natefinch/npipe.v2 v2.0.0-20160621034901-c1b8fa8bdcce // indirect
+	gopkg.in/urfave/cli.v1 v1.20.0 // indirect
 	gopkg.in/yaml.v2 v2.4.0 // indirect
-	gopkg.in/yaml.v3 v3.0.0-20210107192922-496545a6307b // indirect
+	gopkg.in/yaml.v3 v3.0.0 // indirect
 	gotest.tools v2.2.0+incompatible // indirect
 )
