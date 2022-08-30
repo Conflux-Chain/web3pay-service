f222def90485425d3ca10fda658a4872f57498ef
Conflux <conflux@ConfluxdeMacBook-Pro.local>
2022-08-30 14:49:42 +0800

    Monitor APP coin owner changed event

--- blockchain/observer.go
+++ blockchain/observer.go
@@ -16,6 +16,7 @@ type ContractEventObserver interface {
 	OnFrozen(event *contract.APPCoinFrozen, rawlog *types.Log) error
 	OnWithdraw(event *contract.APPCoinWithdraw, rawlog *types.Log) error
 	OnResourceChanged(event *contract.APPCoinResourceChanged, rawlog *types.Log) error
+	OnAppOwnerChanged(event *contract.APPCoinAppOwnerChanged, rawlog *types.Log) error
 	StatusConfirmQueue() <-chan [2]common.Address // [coin, addr]
 	OnConfirmStatus(coin, addr common.Address, balance *big.Int, frozen, block int64) error
 	// airdrop
